cc144f0ac1af22f5052dbd4e4567abf8ffa73ae7
Aditya Vanjari <aditya.vanjari@atyeti.com>
2024-04-16 11:25:55 +0530

    project setup

---
+++ src/main/java/com/example/DatabaseUtility/DatabaseUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.DatabaseUtility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseUtilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DatabaseUtility/DatabaseUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DatabaseUtility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
